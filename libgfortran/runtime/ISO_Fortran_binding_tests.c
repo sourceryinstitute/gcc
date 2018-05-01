e032385dca672f321ae56414497951cba7e105e4
Daniel Celis Garza <celisdanieljr@gmail.com>
2018-05-01 12:04:27 -0700

    Found out how to successfully assign and retrieve memory from the base_addr component of a C descriptor array.

--- libgfortran/runtime/ISO_Fortran_binding_tests.c
+++ libgfortran/runtime/ISO_Fortran_binding_tests.c
@@ -22,10 +22,20 @@ int main (void)
   /* Test function establish. */
   /* Fresh descriptor, base address is NULL. */
   printf ("Test CFI_establish: dv.base_addr == NULL.\n\n");
-  elem_len = 0;
   /* Loop through type. */
   for (int i = 0; i < 10; i++)
     {
+      elem_len = 0;
+      if (type[i] != CFI_type_struct && type[i] != CFI_type_other)
+        {
+          base_type      = type[i] & CFI_type_mask;
+          base_type_size = (type[i] - base_type) >> CFI_type_kind_shift;
+        }
+      else
+        {
+          base_type      = type[i];
+          base_type_size = elem_len;
+        }
       /* Loop through attribute. */
       for (int j = 1; j <= 3; j++)
         {
@@ -35,17 +45,6 @@ int main (void)
             {
               errno = 1;
               rank  = k;
-              if (type[i] != CFI_type_struct && type[i] != CFI_type_other)
-                {
-                  base_type      = type[i] & CFI_type_mask;
-                  base_type_size = (type[i] - base_type) >> CFI_type_kind_shift;
-                }
-              else
-                {
-                  base_type      = type[i];
-                  base_type_size = elem_len;
-                }
-
               CFI_CDESC_T (rank) test1;
               /* We do this because C sometimes doesn't make the structures with
                * a null base_addr which leads to weird behaviour inside
@@ -58,8 +57,9 @@ int main (void)
                 }
               ind = CFI_establish ((CFI_cdesc_t *)&test1, NULL, attribute,
                                    type[i], elem_len, rank, NULL);
-              printf ("attribute = %d\ntype = %d\nbase_type = %ld\nrank = %d\n",
-                      attribute, type[i], base_type, rank);
+              printf ("attribute = %d\ntype = %d\nbase_type = %ld\nrank = "
+                      "%d\nelem_len = %d\n",
+                      attribute, type[i], base_type, rank, elem_len);
               if (ind != CFI_SUCCESS)
                 {
                   printf ("CFI_establish return value = %d\n", ind);
@@ -108,10 +108,20 @@ int main (void)
   /* Fresh descriptor, base address is not NULL */
   printf ("Test CFI_establish: dv.base_addr != NULL.\n\n");
   CFI_index_t *extents = NULL;
-  elem_len             = 0;
   /* Loop through type. */
   for (int i = 0; i < 10; i++)
     {
+      elem_len = 0;
+      if (type[i] != CFI_type_struct && type[i] != CFI_type_other)
+        {
+          base_type      = type[i] & CFI_type_mask;
+          base_type_size = (type[i] - base_type) >> CFI_type_kind_shift;
+        }
+      else
+        {
+          base_type      = type[i];
+          base_type_size = elem_len;
+        }
       /* Loop through attribute. */
       for (int j = 1; j <= 3; j++)
         {
@@ -130,17 +140,6 @@ int main (void)
                 {
                   extents[r] = r + 1;
                 }
-              if (type[i] != CFI_type_struct && type[i] != CFI_type_other)
-                {
-                  base_type      = type[i] & CFI_type_mask;
-                  base_type_size = (type[i] - base_type) >> CFI_type_kind_shift;
-                }
-              else
-                {
-                  base_type      = type[i];
-                  base_type_size = elem_len;
-                }
-
               CFI_CDESC_T (rank) test2;
               /* We do this because C sometimes doesn't make the structures with
                * a null base_addr which leads to weird behaviour inside
@@ -153,8 +152,9 @@ int main (void)
                 }
               ind = CFI_establish ((CFI_cdesc_t *)&test2, &ind, attribute,
                                    type[i], elem_len, rank, extents);
-              printf ("attribute = %d\ntype = %d\nbase_type = %ld\nrank = %d\n",
-                      attribute, type[i], base_type, rank);
+              printf ("attribute = %d\ntype = %d\nbase_type = %ld\nrank = "
+                      "%d\nelem_len = %d\n",
+                      attribute, type[i], base_type, rank, elem_len);
               if (ind != CFI_SUCCESS)
                 {
                   printf ("CFI_establish return value = %d\n", ind);
@@ -236,12 +236,37 @@ int main (void)
 
   /* Fresh descriptor, base address is not NULL */
   printf ("Test CFI_allocate: dv.base_addr != NULL.\n\n");
-  elem_len           = 0;
   CFI_index_t *lower = NULL;
   CFI_index_t *upper = NULL;
   /* Loop through type. */
   for (int i = 0; i < 10; i++)
     {
+      elem_len = 0;
+      if (type[i] == CFI_type_struct)
+        {
+          base_type      = type[i];
+          base_type_size = 69;
+        }
+      else if (type[i] == CFI_type_other)
+        {
+          base_type      = type[i];
+          base_type_size = 666;
+        }
+      else
+        {
+          base_type      = type[i] & CFI_type_mask;
+          base_type_size = (type[i] - base_type) >> CFI_type_kind_shift;
+        }
+
+      elem_len = base_type_size;
+      if (base_type_size == 10)
+        {
+          elem_len = 64;
+        }
+      if (base_type == CFI_type_Complex)
+        {
+          elem_len *= 2;
+        }
       /* Loop through attribute. */
       for (int j = 1; j <= 3; j++)
         {
@@ -272,25 +297,6 @@ int main (void)
                   lower[r]   = r;
                   upper[r]   = lower[r] + extents[r];
                 }
-              if (type[i] != CFI_type_struct && type[i] != CFI_type_other)
-                {
-                  base_type      = type[i] & CFI_type_mask;
-                  base_type_size = (type[i] - base_type) >> CFI_type_kind_shift;
-                }
-              else
-                {
-                  base_type      = type[i];
-                  base_type_size = elem_len;
-                }
-              elem_len = base_type_size;
-              if (base_type_size == 10)
-                {
-                  elem_len = 64;
-                }
-              if (base_type == CFI_type_Complex)
-                {
-                  elem_len *= 2;
-                }
               CFI_CDESC_T (rank) test3;
               /* We do this because C sometimes doesn't make the structures with
                * a null base_addr which leads to weird behaviour inside
@@ -309,7 +315,8 @@ int main (void)
                                    type[i], elem_len, rank, extents);
               ind = CFI_allocate ((CFI_cdesc_t *)&test3, lower, upper,
                                   base_type_size);
-              printf ("type = %ld\nelem_len = %ld\n", base_type, test3.elem_len);
+              printf ("type = %ld\nelem_len = %ld\n", base_type,
+                      test3.elem_len);
               for (int r = 0; r < rank; r++)
                 {
                   if (lower[r] != test3.dim[r].lower_bound)
@@ -336,6 +343,27 @@ int main (void)
                           "%ld\n",
                           i, elem_len);
                   errno *= 5;
+                }
+              if (type[i] == CFI_type_double)
+                {
+                  size_t arr_len = 1;
+                  double val     = 0.;
+                  for (int r = 0; r < rank; r++)
+                    {
+                      arr_len *= test3.dim[r].extent;
+                    }
+                  for (size_t l; l < arr_len; l++)
+                    {
+                      val++;
+                      /* Use memcpy */
+                      memcpy ((char *)test3.base_addr + l * test3.elem_len,
+                              (void *)&val, test3.elem_len);
+                      // printf (" %d ",
+                      //         (char *)test3.base_addr + l * test3.elem_len);
+                      printf(" %f ", *(double*)((char *)test3.base_addr + l * test3.elem_len));
+                      // test3.base_addr[l] = (void*)val;
+                    }
+                  printf ("\n\n");
                 }
               printf ("errno = %d\n", errno);
             }
